349138d597b7ae8aee41a1e457ba51cae2717b6f
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-04-23 13:44:48 +0200

    basic theme toggler

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,39 @@
 
 </script>
 
-<template>
+<script>
+export default {
+  data() {
+    return {
+      darkTheme: false
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.darkTheme = !this.darkTheme;
+    }
+  },
+  watch: {
+    darkTheme: (isDarkTheme) => {
+      document.body.classList = isDarkTheme ? 'dark' : '' ;
+    }
+  },
+  mounted() {
+    document.body.classList = this.darkTheme ? 'dark' : '' ;
+  }
+}
+</script>
 
+<template>
+  <button @click="toggleTheme">Toggle theme</button>
 </template>
 
 <style>
+@import 'https://necolas.github.io/normalize.css/8.0.1/normalize.css';
 @import './assets/base.css';
 
+#app {
+  background-color: var(--main-color);
+}
 
 </style>
